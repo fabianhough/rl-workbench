f00a8e6175aaf2c762d92f90fe1a9b79d9fe100a
Fabian Hough <self@faba.foo>
2026-03-14 21:25:40 -0400

    Changing dimension logic

--- reinforce.py
+++ reinforce.py
@@ -7,19 +7,18 @@ from torch.distributions import Categorical
 
 
 class ModelRLReinforcePolicy(nn.Module):
-    def __init__(self, layer_dims: list):
+    def __init__(self, input_dim: int, output_dim: int, hidden_dims: list, activation=nn.ReLU):
         '''
-        layer_dims (list[int]): List of integers where each integer
-                                    defines a layer size
         '''
         super().__init__()
 
-        # Dynamically add linear projections and adds ReLU activations in between
+        # Dynamically add linear projections and adds activations in between
+        layer_dims = [input_dim] + hidden_dims + [output_dim]
         ops = []
         for i in range(len(layer_dims) - 1): # Iterate two i at a time
             ops.append(nn.Linear(layer_dims[i], layer_dims[i+1]))
             if i < len(layer_dims) - 2:
-                ops.append(nn.ReLU())
+                ops.append(activation())
 
         # Applying operations to policy
         self._policy_params = nn.Sequential(*ops)
