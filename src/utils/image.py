e481aa4050acd52535c03ead770453105cb38978
Fabian Hough <self@faba.foo>
2026-03-17 21:47:02 -0400

    Docstring for save_gif

--- src/utils/image.py
+++ src/utils/image.py
@@ -2,7 +2,16 @@ from PIL import Image
 
 
 
-def save_gif(frames: list, path: str, fps: int=30):
+def save_gif(frames: list, path: str, fps: int=30) -> None:
+    '''
+    Given a list of rgb arrays (frames), creates a .gif
+        at fps and saves to given path
+
+    Parameters:
+        frames (list):  List of RGB arrays
+        path (str):     String path to save .gif
+        fps (int):      Frameratre of .gif    
+    '''
     imgs = [Image.fromarray(f) for f in frames]
     imgs[0].save(
         path, save_all=True, append_images=imgs[1:],
