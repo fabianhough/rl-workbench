708cb92c64bf511aacf81072bb70b8fda26946fb
Fabian Hough <self@faba.foo>
2026-03-18 23:58:53 -0400

    First pass on run script

---
+++ run_reinforce.py
@@ -0,0 +1,44 @@
+
+import yaml
+
+import torch
+import mlflow
+
+from src.agents import AgentReinforce
+from src.learn import train, TrainFreq, SamplingType
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    with open('reinforce-config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    mlflow.set_tracking_uri(config.pop('tracking_uri'))
+    mlflow.set_experiment(config.pop('experiment'))
+    
+    # Generating Environment
+    env = gym.make(id=config.pop('env_str'))
+
+    # Generating agent
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    agent = AgentReinforce(
+        input_dim=env.observation_space.shape[0],
+        output_dim=env.action_space.n,
+        hidden_dims=config.pop('hidden_dims'),
+        device=device,
+        gamma=config.pop('gamma'),
+        lr=config.pop('lr')
+    )
+
+    raise Exception
+
+    # Train
+    config['train_freq'] = TrainFreq(config['train_freq'])
+    config['sampling'] = SamplingType(config['sampling'])
+    with mlflow.start_run():
+        train(agent=agent, env=env, **config)
