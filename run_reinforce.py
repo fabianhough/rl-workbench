8c24e9742dc39f5b58a11791888ca45c08872f99
Fabian Hough <self@faba.foo>
2026-03-19 00:26:09 -0400

    Refining logging and comments

--- run_reinforce.py
+++ run_reinforce.py
@@ -23,23 +23,30 @@ if __name__ == '__main__':
     # Configuring mlflow
     mlflow.set_tracking_uri(config.pop('tracking_uri'))
     mlflow.set_experiment(config.pop('experiment'))
-    
-    # Generating Environment
-    env = gym.make(id=config.pop('env_str'), render_mode='rgb_array')
 
-    # Generating agent
-    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-    agent = AgentReinforce(
-        input_dim=env.observation_space.shape[0],
-        output_dim=env.action_space.n,
-        hidden_dims=config.pop('hidden_dims'),
-        device=device,
-        gamma=config.pop('gamma'),
-        lr=config.pop('lr')
-    )
-
-    # Train
-    config['train_freq'] = TrainFreq(config['train_freq'])
-    config['sampling'] = SamplingType(config['sampling'])
+    # Starting MLFlow
     with mlflow.start_run():
+
+        # Log Parameters
+        mlflow.log_params(params=config)
+    
+        # Generating Environment
+        env = gym.make(id=config.pop('env_str'), render_mode='rgb_array')
+
+        # Generating agent
+        device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+        agent = AgentReinforce(
+            input_dim=env.observation_space.shape[0],
+            output_dim=env.action_space.n,
+            hidden_dims=config.pop('hidden_dims'),
+            device=device,
+            gamma=config.pop('gamma'),
+            lr=config.pop('lr')
+        )
+
+        # Setting up training frequency and sampling type
+        config['train_freq'] = TrainFreq(config['train_freq'])
+        config['sampling'] = SamplingType(config['sampling'])
+
+        # Train
         train(agent=agent, env=env, **config)
