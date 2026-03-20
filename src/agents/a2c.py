a7e03177357a9cc6c7aadc1e419a1cac99797546
Fabian Hough <self@faba.foo>
2026-03-19 21:29:15 -0400

    Adding training function

--- src/agents/a2c.py
+++ src/agents/a2c.py
@@ -20,11 +20,17 @@ class AgentA2C(SimpleLinearNet):
         device='cpu',
         policy_lr=3e-4,
         value_lr=1e-3,
-        gamma=0.99
+        gamma=0.99,
+        critic_coeff=0.5,
+        entropy_coeff=0.05
     ):
         
         self._device = device
         self._env = env
+
+        self.gamma = gamma
+        self.critic_coeff = critic_coeff
+        self.entropy_coeff = entropy_coeff
 
         self.policy_net = SimpleLinearNet(
             input_dim=input_dim,
@@ -49,13 +55,58 @@ class AgentA2C(SimpleLinearNet):
             lr=value_lr
         )
 
+    def act(self, observ, **kwargs):
+        self.policy_net.eval()
+        with torch.no_grad():
+            # Prepare observ
+            observ_tensor = torch.tensor(observ, dtype=torch.float32).to(self._device)
 
-
-    def act(self, observ, **kwargs): ...
-
+            # Sample an action
+            action = self.policy(observ_tensor.unsqueeze(0)).sample().item()
+        self.policy_net.train()
+        return action
 
     def train(self, sample): ...
+        # Unwrapping sample
+        observs, actions, rewards, next_observs, dones = sample
 
+        # Preparing tensors
+        observs = torch.tensor(observs).to(self._device)
+        actions = torch.tensor(actions).to(self._device)
+        rewards = torch.tensor(rewards).to(self._device)
+        next_observs = torch.tensor(next_observs).to(self._device)
+        dones = torch.tensor(dones).to(self._device)
+
+        # Preparing optimizer
+        self.policy_optimizer.zero_grad()
+        self.value_optimizer.zero_grad()
+
+        # Critic
+        critic_td = rewards + self.gamma * (1 - dones) * value_net(next_observs)
+        critic_y_h = value_net(observs)
+
+        # Actor
+        advantage = (critic_td - critic_y_h).detach()
+        actor_dist = policy_net.policy(observs)
+        actor_log_prob = actor_dist.log_prob(actions)
+        actor_entropy = actor_dist.entropy()
+
+        # Calculating loss
+        critic_loss = F.mse_loss(critic_y_h, critic_td.detach())
+        actor_loss = -(actor_log_prob * advantage).mean() - self.entropy_coeff * actor_entropy.mean()
+        loss = actor_loss + self.critic_coeff * critic_loss
+
+        # Back-propagation
+        loss.backward()
+        self.policy_optimizer.step()
+        self.value_optimizer.step()
+
+        # Returning loss metrics
+        return {
+            'loss': loss.item(),
+            'actor_loss': actor_loss.item()
+            'critic_loss': critic_loss.item()
+        }
 
     def post_episode(self): ...
 
